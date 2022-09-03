162c94a2e51557a715ecbdb30c4f92f1d5089099
Emmanuel Martinez <emmamartinzwc@gmail.com>
2022-09-03 16:28:48 -0300

    OK: setups express server with file upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+
+const app = express();
+const port = 3000;
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Upload files");
+});
+
+app.post("/files", (req, res) => {
+  console.log(req.files);
+});
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
