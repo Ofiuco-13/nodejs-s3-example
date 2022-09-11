9f371b50b6313d0edb4e101d4052dfe31dcb2b4c
Emmanuel Martinez <emmamartinzwc@gmail.com>
2022-09-11 20:04:21 -0300

    FEATURE:now can upload files with s3 sdk

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import fileUpload from "express-fileupload";
-import "./config.js";
+import { uploadFile } from "./s3.js";
 
 const app = express();
 const port = 3000;
@@ -16,8 +16,9 @@ app.get("/", (req, res) => {
   res.send("Upload files");
 });
 
-app.post("/files", (req, res) => {
-  console.log(req.files);
+app.post("/files", async (req, res) => {
+  const result = await uploadFile(req.files.file);
+  res.send(result);
 });
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
